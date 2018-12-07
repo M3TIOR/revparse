742c375be93113919bfe909f016c70d260a32990
M3TIOR <jtimmerman32@gmail.com>
2018-12-06 20:39:18 -0800

    started writing library. This project is on hiatus. I actually can't remember why I started writing it in the first place...
    ALSO... OS refresh prep

---
+++ src/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+
+# M3TIOR 2017
+#
+#
+
+from argparse import (
+	
+)
+
+__all__ = [
+	'ArgumentParser',
+	'ArgumentError',
+	'ArgumentTypeError',
+	'FileType',
+	'HelpFormatter',
+	'ArgumentDefaultsHelpFormatter',
+	'RawDescriptionHelpFormatter',
+	'RawTextHelpFormatter',
+	'MetavarTypeHelpFormatter',
+	'Namespace',
+	'Action',
+	'ONE_OR_MORE',
+	'OPTIONAL',
+	'PARSER',
+	'REMAINDER',
+	'SUPPRESS',
+	'ZERO_OR_MORE',
+]
+
+def ArgumentParser():
+	"""
+	"""
